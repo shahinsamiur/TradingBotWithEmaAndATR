2fcb3b5a84c8e29b24e1b5337b4100051ded39a8
shahinsamiur <shahinsamiur@yahoo.com>
2025-08-04 22:34:43 +0600

    test telegram messages added

--- businesslogic/logic.py
+++ businesslogic/logic.py
@@ -1,5 +1,6 @@
 from indicatios.atr import atr_stoploss
 from indicatios.ema import ema
+from sendMessage.testSendMessage import send_message
 import sys
 import logging
 def botLogic(marketData):
@@ -12,15 +13,24 @@ def botLogic(marketData):
     ema50_curr =ema({symbol: marketData[symbol].iloc[:-1]}, symbol, 'close', 50)
     ema21_curr = ema({symbol: marketData[symbol].iloc[:-1]}, symbol, 'close', 21)
 
-    logging.info(f"ema21: curr={ema21_curr}, prev={ema21_prev}")
-    logging.info(f"ema50: curr={ema50_curr}, prev={ema50_prev}")
+    # logging.info(f"ema21: curr={ema21_curr}, prev={ema21_prev}")
+    # logging.info(f"ema50: curr={ema50_curr}, prev={ema50_prev}")
+    atr_buy=atr_stoploss(data=marketData[symbol] , newTrend="buy_signal")
+    atr_sell=atr_stoploss(data=marketData[symbol] , newTrend="sell_signal")
+    # buy_sl = atr_stoploss(data=marketData[symbol], newTrend="buy_signal")
+    # logging.info(f"Buy SL: {buy_sl}")
 
-    buy_sl = atr_stoploss(data=marketData[symbol], newTrend="buy_signal")
-    logging.info(f"Buy SL: {buy_sl}")
+    # sell_sl = atr_stoploss(data=marketData[symbol], newTrend="sell_signal")
+    # logging.info(f"Sell SL: {sell_sl}")
+    message = (
+        f"📊 *Trend Detection Update:*\n\n"
+        f"*EMA 21* ➤ Prev: `{ema21_prev:.2f}`, Curr: `{ema21_curr:.2f}`\n"
+        f"*EMA 50* ➤ Prev: `{ema50_prev:.2f}`, Curr: `{ema50_curr:.2f}`\n\n"
+        f"🛒 *ATR Stop Loss (Buy)*: `{atr_buy:.2f}`\n"
+        f"🛑 *ATR Stop Loss (Sell)*: `{atr_sell:.2f}`"
+    )
 
-    sell_sl = atr_stoploss(data=marketData[symbol], newTrend="sell_signal")
-    logging.info(f"Sell SL: {sell_sl}")
-
+    send_message(message)
 
 
 
